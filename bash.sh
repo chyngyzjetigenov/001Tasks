fef39487413fda5dfaaa866de3aaed55cd9c3d9e
chyngyzjetigenov <117899294+chyngyzjetigenov@users.noreply.github.com>
2023-02-13 23:39:22 +0600

    Update bash.sh
    Start automatically

--- bash.sh
+++ bash.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-ssh -T jetigenov@192.168.1.112 -S "sudo docker build -t mynewapp . && sudo docker run -dp 8080:8080 mynewapp"
+ssh -T jetigenov@192.168.1.112 -S "sudo docker build -t mynewapp . && sudo docker run -dp --restart=always 8080:8080 mynewapp"
